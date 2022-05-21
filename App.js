b69803b6f78fe5c6f909271cd724fa7d5a0b2003
KavishanSukumar <kavishansukumar@gmail.com>
2022-05-21 20:32:24 +0530

    app js keyboard issue

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { KeyboardAvoidingView, Platform } from "react-native";
+import { KeyboardAvoidingView, Platform, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
@@ -17,7 +17,7 @@ export default function App() {
           <KeyboardAvoidingView
             behavior={Platform.OS === "ios" ? "padding" : "height"}
             style={{ flex: 1 }}
-            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : -64}
           >
             <Stack.Navigator>
               <Stack.Screen
